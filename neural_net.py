cb0d77320ede7562494f5a88604d877e9796dd35
Ronnypetson <rsronnypetson4@gmail.com>
2018-09-28 15:53:06 -0300

    neural net

---
+++ neural_net.py
@@ -0,0 +1,94 @@
+import numpy as np
+
+# Ativações
+def relu(z):
+	return np.array([max(v,0.0) for v in z])
+
+def sigmoid(z):
+	return 1.0/(1.0+np.exp(-z))
+
+# Derivadas das ativações
+def relu_(z):
+	return np.array([1.0 if v > 0 else 0.0 for v in z])
+
+def sigmoid_(z):
+	g = self.sigmoid(z)
+	return g*(1.0-g)
+
+acts = {"sigmoid":sigmoid,"relu":relu}
+acts_ = {"sigmoid":sigmoid_,"relu":relu_}
+
+class layer:
+	def __init__(self,input_len,output_len,activation):
+		self.shape = (input_len,output_len)
+		self.activation = activation
+		self.theta = np.random.uniform(-1.0,1.0,size=self.shape)
+
+class neural_net:
+	# Initializer
+	# O primeiro neuronio de cada camada é o bias
+	def __init__(self,learning_rate=0.1,train_iter=1000,mini_batch_len=100,layers_dims=[(785,100),(100,10)],\
+							 activation="sigmoid"):
+		self.learning_rate = learning_rate
+		self.train_iter = train_iter
+		self.mini_batch_len = mini_batch_len
+		self.num_classes = layers_dims[-1][1]
+		self.layers = []
+		for d in layers_dims:
+			self.layers.append(layer(d[0],d[1],activation))
+
+	def softmax(self,y):
+		return np.exp(y)/np.sum(np.exp(y))
+
+	def cross_entropy_loss(self,y,y_):
+		return -np.dot(y,np.log(y_))
+
+	def loss(self,y,y_):
+		return -np.add(np.dot(y,np.log(y_))+np.dot((1.0-y),(np.log(1.0-y_))))
+
+	def forward(self,X):
+		a = [X] # Atentar para os índices: a[i], theta[i] -> z[i] -> a[i+1]
+		z = [ np.dot(self.layers[0].theta,a[0]) ]
+		for i in range(1,len(self.layers)):
+			a.append(acts[activation](z[i-1]))
+			a[i][0] = 1.0
+			z.append(np.dot(self.layers[i].theta,a[i]))
+		a.append(acts[activation](z[-1])) # Tem que guardar a ativação do final
+		return a, acts_[activation](z)
+
+	# Fit parameters theta by mini-batch gradient descent
+	def fit(self,X_train,Y_train):
+		# Initialize parameters with small random values
+		mean_losses = np.zeros(self.train_iter)
+		# Update parameters iteratively
+		for i in range(self.train_iter):
+			#grad = np.zeros(self.theta.shape)
+			indices = np.random.choice(X_train.shape[0],self.mini_batch_len,replace=False)
+			deltas = []
+			for j in range(len(self.layers)):
+				deltas.append(np.zeros(self.layers[j].shape))
+			# Compute gradient
+			for ind in indices:
+				x = X_train[ind]
+				y = np.zeros(self.num_classes)
+				y[int(Y_train[ind])] = 1.0
+				act,act_ = self.forward(x)
+				erro = [act[-1]-y]
+				for j in range(len(self.layers)-2,0,-1):
+					erro.append( np.dot(self.layers[j].theta[1:],erro[-1]).multiply(act_[j]) )
+				for j in range(len(self.layers)):
+					deltas[j] = np.add(deltas[j],np.outer(erro[len(self.layers)-j],act[j]))
+				mean_losses[i] += self.cross_entropy_loss(y,act[-1]) # changed here
+			# Compute gradients
+			deltas = [d*self.learning_rate/self.mini_batch_len for d in deltas]
+			# Update parameters
+			for j in range(len(self.layers)):
+				self.layers[j].theta -= deltas[j]
+			mean_losses[i] /= self.mini_batch_len
+		return mean_losses
+
+	# Estimate Y from X
+	def predict(self,X_test):
+		#return [np.argmax(self.softmax(np.dot(x,self.theta))) for x in X_test]
+		return forward(X_test)[0]
+
--- validation.py
+++ validation.py
@@ -2,6 +2,7 @@ import numpy as np
 from preprocess import *
 from log_reg import *
 from multi_log_reg import *
+from neural_net import *
 from matplotlib import pyplot as plt
 
 def mse(Y,Y_):
@@ -52,6 +53,22 @@ def multi_log_reg_(train_X,train_Y,valid_X,valid_Y,num_classes=10,learning_rate=
 		one_hot_Y.append(y)
 	return mse_(np.array(one_hot_Y),Y_), lr_model.theta
 
+def neural_net_(train_X,train_Y,valid_X,valid_Y,learning_rate=0.1,max_iter=500):
+	## Descida de gradiente
+	lr_model = neural_net(learning_rate=learning_rate,train_iter=max_iter)
+	cost = lr_model.fit(train_X,train_Y)
+	plt.xlabel('iterations')
+	plt.ylabel('Cost')
+	plt.plot(cost)
+	plt.show()
+	#Y_ = lr_model.predict(valid_X)
+	#one_hot_Y = []
+	#for i in range(valid_Y.shape[0]):
+	#	y = np.zeros(num_classes)
+	#	y[int(valid_Y[i])] = 1.0
+	#	one_hot_Y.append(y)
+	#return mse_(np.array(one_hot_Y),Y_), lr_model.theta
+
 ## Read data from csv files
 # Load train-validation
 train_X,train_Y = getXY('fashion-mnist-dataset/fashion-mnist_train.csv')
@@ -62,21 +79,23 @@ test_X,test_Y = getXY('fashion-mnist-dataset/fashion-mnist_test.csv')
 num_samples = train_X.shape[0]
 num_features = train_X.shape[1]
 k = 2
-chosen_params = {"RegressaoLogistica_OneVsAll":np.zeros((num_features,10)), "RegressaoLogistica_Multiclasse":np.zeros((num_features,10))}
+chosen_params = {"RedeNeural":neural_net_,"RegressaoLogistica_OneVsAll":np.zeros((num_features,10)), "RegressaoLogistica_Multiclasse":np.zeros((num_features,10))}
 block_len = int(num_samples/k)
-methods = {"RegressaoLogistica_Multiclasse":multi_log_reg_} # "RegressaoLogistica_OneVsAll":log_reg_
+methods = {"RedeNeural":neural_net_} # "RegressaoLogistica_OneVsAll":log_reg_, "RegressaoLogistica_Multiclasse":multi_log_reg_
 for m in methods:
 	print("Evaluating method "+m)
 	mean_losses = np.zeros(k)
-	params = np.zeros((k,num_features,10))
+	#params = np.zeros((k,num_features,10))
 	for i in range(k):
 		_valid_X = train_X[i*block_len:(i+1)*block_len]
 		_train_X = np.concatenate((train_X[0:i*block_len],train_X[(i+1)*block_len:]),axis=0)
 		_valid_Y = train_Y[i*block_len:(i+1)*block_len]
 		_train_Y = np.concatenate((train_Y[0:i*block_len],train_Y[(i+1)*block_len:]),axis=0)
-		mean_losses[i], params[i] = methods[m](_train_X,_train_Y,_valid_X,_valid_Y) # params[i]
-	print("Validation mean loss score %s: %f"%(m,np.mean(mean_losses)))
-	chosen_params[m] = params[np.argmin(mean_losses)]
+		methods[m](_train_X,_train_Y,_valid_X,_valid_Y)
+		#mean_losses[i], params[i] = methods[m](_train_X,_train_Y,_valid_X,_valid_Y) # params[i]
+	#print("Validation mean loss score %s: %f"%(m,np.mean(mean_losses)))
+	#chosen_params[m] = params[np.argmin(mean_losses)]
 
 #for m in methods:
 #	print("Test mean loss score: %f"%mse(test_Y, 1.0/(1.0+np.exp(-np.dot(test_X,chosen_params[m])))))
+
